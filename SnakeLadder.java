68735b3f7b05f804bb0255fed520c6f17c5773f4
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-02 15:52:01 +0530

    added conditions for snake and ladder

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,13 +1,46 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class SnakeLadder{
 
+	public static final int LADDER=1; 
+	public static final int SNAKE=2;
+	public static final int COUNTER=99;
+	
+	
+	
 	public static void main(String[] args){
 	
-		int startPosition=0;
-		System.out.println(startPosition);
-		
-		int dice=(int)(Math.random()*6+1);
-		System.out.println("Dice no: " + dice);
-		
-	}
+	
+		int startPosition=0, playerPos=0, numOfPlay=0;
+		//System.out.println(startPosition);
 
+		//System.out.println("Dice no: " + dice);
+			
+		while (playerPos < COUNTER){
+				
+				numOfPlay++;
+				int dice=(int)(Math.random()*6+1);
+				boolean ladder = Math.random()<0.5;
+				boolean snake= Math.random()<0.5;
+				
+				if (ladder == true){
+						
+					playerPos += dice;
+				}
+							
+				else if (snake == true){
+					playerPos -= dice;
+				}
+					
+			}
+			
+			
+		System.out.println("Number of plays: " + numOfPlay);
+		System.out.println("player position: " + playerPos);
+			
+	}
+			
+		
 }
+
